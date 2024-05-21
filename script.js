f6f5f52109ac28aa265862bc13dbb6d6472769a1
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-05-21 16:55:29 -0400

    Initial commit

--- script.js
+++ script.js
@@ -22,14 +22,17 @@ const weapons = [
     name: "stick",
     power: 5
   },
+
   {
     name: "dagger",
     power: 30
   },
+
   {
     name: "claw hammer",
     power: 50
   },
+  
   {
     name: "sword",
     power: 100
@@ -42,11 +45,13 @@ const monsters = [
     level: 2,
     health: 15
   },
+
   {
     name: "fanged beast",
     level: 8,
     health: 60
   },
+  
   {
     name: "dragon",
     level: 20,
